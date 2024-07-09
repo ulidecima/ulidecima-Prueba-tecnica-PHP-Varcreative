c5c8bb4f762f6af920f089620b51dc54ba966c6e
Ulises Decima <ulidecima1505@gmail.com>
2024-07-08 23:29:55 -0300

    fix: Corrige un link roto en footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -4,7 +4,7 @@
                 <div class="container">
                     © 2024 Varcreative. All rights reserved. 
                     <a class="grey-text text-lighten-4 right" href="https://www.varcreative.com/">Varcreative</a><br>
-                    <a class="grey-text text-lighten-4 right" href="www.linkedin.com/in/ulises-decima-704b28261">Ulises Décima</a>
+                    <a class="grey-text text-lighten-4 right" href="https://www.linkedin.com/in/ulises-decima-704b28261">Ulises Décima</a>
                 </div>
             </div>
         </footer>
